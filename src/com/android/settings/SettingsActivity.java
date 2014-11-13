5ec41e263a09a2555046ceeecb77da61640290ab
Adnan <adnan@cyngn.com>
2014-11-12 17:14:29 -0800

    Blacklist: Fix launching blacklist settings
    Change-Id: I0043503e6ac0d3536941394ee5057001b72f9114
    
    Conflicts:
    	src/com/android/settings/Settings.java
    	src/com/android/settings/SettingsActivity.java

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -73,6 +73,7 @@ import com.android.settings.applications.ExpandedDesktopPreferenceFragment;
 import com.android.settings.applications.InstalledAppDetails;
 import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ProcessStatsUi;
+import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardSummary;
@@ -310,6 +311,8 @@ public class SettingsActivity extends Activity
             ApnSettings.class.getName(),
             LiveDisplay.class.getName(),
             ExpandedDesktopPreferenceFragment.class.getName()
+            LiveDisplay.class.getName()
+            BlacklistSettings.class.getName()
     };
 
 
